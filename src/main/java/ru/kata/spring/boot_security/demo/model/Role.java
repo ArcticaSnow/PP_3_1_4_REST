f3ac8b80ee6a80468e98eff1513e39f8b0a76d1c
ArcticaSnow <katy.shamina@yandex.ru>
2022-11-01 09:07:59 +0300

    First commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,62 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    public Role() {
+
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name.replaceAll("ROLE_", "");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return id == role.id && name.equals(role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+
+        result = 31 * result * name.hashCode();
+
+        return result;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -0,0 +1,116 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import lombok.Data;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(unique = true, name = "user_name")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_roles")
+    private List<Role> roles;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, Integer age, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.username = username;
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        if (roles == null) {
+            roles = new LinkedList<>();
+        }
+        return roles;
+    }
+
+    public String getRolesToString() {
+        StringBuilder sb = new StringBuilder();
+        for (Role role : roles) {
+            sb.append(role.toString()).append(" ");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id == user.id && username.equals(user.username) && firstName.equals(user.firstName) &&
+                lastName.equals(user.lastName) && age == user.age && password.equals(user.password) &&
+                roles.equals(user.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (id ^ (id >>> 32));
+
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + roles.hashCode();
+
+        return result;
+    }
+
+    @Override
+    public List<Role> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
